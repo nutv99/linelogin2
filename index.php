5bcd0e92eb6c6f1886951aae48a94c62a3ff30aa
nutv99 <nutv99@gmail.com>
2022-02-17 13:05:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $userID = $sValue[2] ;
 
 $contact9 = $sValue[0] ;
 
-pushMessage($contact9,$access_token,$replyToken) ;  return ;
+//pushMessage($contact9,$access_token,$replyToken) ;  return ;
 
 //$result = getPortImageURL($contact9,$userID) ;
 //$str    = getPortDataString($contact9) ;
@@ -31,6 +31,24 @@ if (trim($resultAr[0]) == "Fail") {
 
 function getInputMessage() { 
 
+  $content = file_get_contents('php://input');
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'] ;  		
+			$sValue[] = $text;
+			$sValue[] = $replyToken ;  
+			$sValue[] = $event['source']['userId'];  
+			return $sValue;			
+		}  //  end if
+	} // end for
+}
 
  
 } // end func 
