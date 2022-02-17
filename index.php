39cbd7a25489f6d736a23e44217353787a37d764
nutv99 <nutv99@gmail.com>
2022-02-17 12:56:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,19 +16,17 @@ $str    = getPortDataString($contact9) ;
 $resultAr = explode("|",$result); 
 
 
-
 if (trim($resultAr[0]) == "Fail") {
   pushMessage($resultAr[1],$access_token,$replyToken) ; 
   return;
 }  else {
   $ImageFileName = $result ; 
-  pushImage($ImageFileName,$access_token,$replyToken);
-  
-  pushMessage($str,$access_token,$replyToken) ; 
+  pushImage($ImageFileName,$access_token,$replyToken);  
+  //pushMessage($str,$access_token,$replyToken) ; 
 }
 
-//pushMessage($result,$access_token,$replyToken) ; 
-echo " Curl Result-->" . $result ;
+
+
 return;
  
  
