396c685fb6e204c8fcd0660eb6492101eeda3e85
nutv99 <nutv99@gmail.com>
2022-02-17 13:00:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $access_token = 'N0IzKf3n/tuu23eKxvUEkAY6Afzj8nu+lQYp+FyOAZXSVofsrCArcwRBOJKEbssASNnN5S35vUE5yiQ3dPcvlRqu9G0IVPHVxUHUHW63dUUUdxfcWpbZUj7iu8ImPFKK8LnAdy5wGDxvMhUD1A1fugdB04t89/1O/w1cDnyilFU='; 
 
 
-$sValue= getInputMessage() ;
+//$sValue= getInputMessage() ;
 $MessageInput = $sValue[0];  
 $replyToken =  $sValue[1];  
 $userID = $sValue[2] ;
@@ -30,30 +30,4 @@ if (trim($resultAr[0]) == "Fail") {
 return;
  
  
-function getInputMessage() { 
-
-
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'] ;  
-		
-			$sValue[] = $text;
-			$sValue[] = $replyToken ;  
-			$sValue[] = $event['source']['userId'];  
-			return $sValue;
-			
-		}
-	}
-}
-    
-} // end func 
+ 
